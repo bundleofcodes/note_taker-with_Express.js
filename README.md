89dadea125e49566fee06040daf4f5d0e9a7387d
Raiya Young <121911949+bundleofcodes@users.noreply.github.com>
2023-04-11 23:14:21 -0400

    edited code for delete function and added README.md

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # note_taker-with_Express.js
+
+## Description
+
+The purpose of this project was to create an application that has the ability to create, save and delete notes with a note taker. This application uses Express.js on the back end and saves and retieves user generated note data from JSON file. The Note Taker application allows you to enter in the title and text on your note taker app and save the items as a way to keep track of tasks. The application's front end was previously created and as a developer, it was my responsibility to build the back end and connect the two, then deploy completed application to Heroku. 
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](credits)
+- [License](#license)
+- [Tests](#tests)
+- [Features](#features)
+- [Questions](#questions)
+
+## Installation:
+
+- npm i
+- Node.js
+- express.js
+
+## Usage:
+
+- Application Screenshot:
+
+- ![Application Screenshot](./README%20preview%20Screenshot.png)
+
+- [Repository URL](https://github.com/bundleofcodes/note_taker-with_Express.js)
+
+## Credits:
+
+- Raiya Young
+- Georgia Tech Coding Bootcamp
+- Mazin Abed
+- Youtube.com
+- Pham Ngan
+- Jacob Willis
+
+## License:
+
+This project is licensed under MIT License. Click [here](https:)
+
+## Tests:
+
+No tests available
+
+## Features:
+
+- Saved Task Notes
+
+## Questions:
+
+If you have questions about this repo, open an issue or contact me directly at realdeveloper@gmail.com. You can also find more of my work at [Bundleofcodes](https://github.com/bundleofcodes)
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const allNotes = 'db/db.json'
 const app = express();
 const PORT = process.env.PORT || 3001;
-const allNotes = require('/Users/cmitchell/bootcamp/GT-VIRT-FSF-PT-01-2023-U-LOLC/note_taker-with_Express.js/db/db.json');
 const { text } = require('express');
 const { title } = require('process');
 const shortid = require('shortid');
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('/Users/cmitchell/bootcamp/GT-VIRT-FSF-PT-01-2023-U-LOLC/note_taker-with_Express.js/db/db.json', (err, data) => {
+    fs.readFile('db/db.json', (err, data) => {
         const allData = JSON.parse(data)
-    res.json(allData.slice());
+        res.json(allData.slice());
     });
 });
 
@@ -33,33 +33,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// function createNewNote(body, notesArray) {
-//     const newNote = {
-//         title: title.body,
-//         text: text.body,
-//         id: shortid.generate()
-//     };
-//     // if (!Array.isArray(notesArray))
-//     //     notesArray = [];
 
-//     // if (notesArray.length === 0)
-//     //     notesArray.push(0);
-
-//     // body.id = notesArray[0];
-//     // notesArray[0]++;
-
-//     notesArray.push(newNote);
-//     fs.writeFileSync(
-//         path.join(__dirname, './db/db.json'),
-//         JSON.stringify(notesArray, null, 2)
-//     );
-//     return newNote;
-// }
-//Help from Pham
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
     const id = shortid.generate()
-    fs.readFile('/Users/cmitchell/bootcamp/GT-VIRT-FSF-PT-01-2023-U-LOLC/note_taker-with_Express.js/db/db.json', (err, data) => {
+    fs.readFile('db/db.json', (err, data) => {
         const allData = JSON.parse(data)
         const newData = {
             title,
@@ -67,37 +45,43 @@ app.post('/api/notes', (req, res) => {
             id
         }
         allData.push(newData)
-        fs.writeFile('/Users/cmitchell/bootcamp/GT-VIRT-FSF-PT-01-2023-U-LOLC/note_taker-with_Express.js/db/db.json', JSON.stringify(allData,null,2), (err) => {
-            if (err)throw err;
-            res.send("200"); 
-            // console.log(err)
+        fs.writeFile('db/db.json', JSON.stringify(allData, null, 2), (err) => {
+            if (err) throw err;
+            res.send("200");
         })
-        })
-        // const newNote = createNewNote(req.body, allNotes);
+    })
+
+});
+
+
+function deleteNote(id, callback, callbackString) {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+      if (err) {
+        callback(err);
+      } else {
+        const notes = JSON.parse(data);
+        const updatedNotes = notes.filter(note => note.id !== id);
+        fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(updatedNotes, null, 2), err => {
+          if (err) {
+            callback(err);
+          } else {
+            callbackString('Note deleted successfully!');
+          }
+        });
+      }
     });
-    //Help ended from Pham
-    // const newNote = createNewNote(req.body, allNotes);
-    // res.json(newNote);
-
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, '/Users/cmitchell/bootcamp/GT-VIRT-FSF-PT-01-2023-U-LOLC/note_taker-with_Express.js/db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
-        }
-    }
-}
+  }
 
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.send("200");
+    deleteNote(req.params.id, (err) => {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send("200");
+        }
+    }, (message) => {
+        res.send(message);
+    });
 });
 
 app.listen(PORT, () => {
